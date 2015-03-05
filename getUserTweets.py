b4bbe35c542ff76baebb9d916fed4a4edad1d934
Masha Westerlund <masha.westerlund@gmail.com>
2015-03-05 16:57:23 -0500

    modified git ignore, added user tweet capture

---
+++ getUserTweets.py
@@ -0,0 +1,62 @@
+import tweepy
+import os
+import json
+
+def getUserTweets(twitter_handle):
+
+	auth = tweepy.OAuthHandler(os.environ['consumer_token'], os.environ['consumer_secret'])
+	auth.set_access_token(os.environ['access_token'], os.environ['access_secret'])
+	api = tweepy.API(auth)
+
+	user_tweets = []
+
+	# make initial request, only allowed 200 at a time
+
+	new_tweets = api.user_timeline(screen_name = twitter_handle, count=200)
+	no_retweets = filterRetweets(new_tweets)
+	user_tweets += no_retweets
+
+	#get id of the oldest tweet, to use as next request placeholder
+
+	oldest = user_tweets[-1].id -1
+
+	#then keep going through all the tweets that twitter lets you take
+
+	while len(new_tweets) >0 :
+		new_tweets = api.user_timeline(screen_name = twitter_handle, count =200, max_id = oldest)
+		no_retweets = filterRetweets(new_tweets)
+		user_tweets += no_retweets
+		oldest = user_tweets[-1].id -1
+
+	# get only the text
+
+	tweet_text = outputText(user_tweets)
+
+	return tweet_text
+
+def filterRetweets(tweets):
+	clean_tweets = []
+	for tweet in tweets:
+		if 'retweeted_status' not in tweet._json.keys():
+			clean_tweets.append(tweet)
+	return clean_tweets
+
+def outputText(tweets):
+	tweet_text = []
+	for tweet in tweets:
+		tweet_text.append(tweet._json['text'])
+	return tweet_text
+
+def cleanTweetText(tweet_text):
+	pass
+
+
+
+if __name__ == '__main__':
+	getUserTweets('mallelis')
+
+
+
+
+
+
